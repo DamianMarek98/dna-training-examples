401de5519c90808ed456595a8fc8117d078940e2
damianm <damian.marek@incata.it>
2024-01-10 00:09:38 +0100

    add more info about command bus

--- demo/src/main/java/dna/example/demo/construction/elements/examples/SpecificCommandHandlerImplementation.java
+++ demo/src/main/java/dna/example/demo/construction/elements/examples/SpecificCommandHandlerImplementation.java
@@ -5,5 +5,7 @@ class SpecificCommandHandlerImplementation implements CommandHandler<Command> {
     @Override
     public void handle(Command command) {
         //this can work as domain service
+        //todo we need commandBus which takes care of triggering handlers -> we can register or lookup for beans which are command handlers
+        //look for beans that are annotated for example
     }
 }
